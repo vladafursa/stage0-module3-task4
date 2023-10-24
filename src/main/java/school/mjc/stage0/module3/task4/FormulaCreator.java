0284fc0030f9476278189591a0bb7a7bb547a9fb
T0337485 <t0337485@my.ntu.ac.uk>
2023-10-25 00:18:13 +0100

    fixed

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a;
-        int b;
+        int a=1;
+        int b=2;
         float result=(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
     }
 }
